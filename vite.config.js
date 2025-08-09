c6fce39cdcd057596f64fd2b8eb4182397d5dcba
Horse Writer (GhostWriter) <88307076+horsewriter@users.noreply.github.com>
2025-08-09 19:54:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['bcryptjs'],
+      external: ['bcryptjs'], // Evita que lo empaquete
     },
   },
-};
+})
